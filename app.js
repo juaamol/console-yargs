fca847de90cc0e8129555d0fe50666267e643c1a
juaamol <jambulud@gmail.com>
2019-05-29 19:43:14 +0200

    [UPDATE] - receive base from terminal

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const { createFile } = require('./multiplication/multiplication');
+let receivedArgv = process.argv;
 
-let base = 'aaa';
-
+if (receivedArgv < 3) {
+    console.log('You have to write the base');
+    exit(1);
+}
+let receivedArg = receivedArgv[2];
+console.log(receivedArg.split('='));
+let base = Number(receivedArg.split('=')[1]);
 
 createFile(base)
     .then((filename) => console.log(`File ${filename} created`))
